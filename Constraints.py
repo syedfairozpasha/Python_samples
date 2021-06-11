a88abd2efd710e1a5b63e1e88de6640899c9d67f
sfp_BrainFry <49707987+syedfairozpasha@users.noreply.github.com>
2021-06-12 00:33:40 +0530

    range added

---
+++ Constraints.py
@@ -0,0 +1,8 @@
+baseset ="characterset"
+subset ="CHARACTER"
+if subset and subset.casefold() in baseset.casefold():
+    print("Gone")
+else:
+    print("present")
+for itr in range(-100,10,5):
+    print(itr)
