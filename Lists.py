30df5a958e1f951f31b6b4f331f655a240593a2c
sfp_BrainFry <49707987+syedfairozpasha@users.noreply.github.com>
2021-06-13 00:02:40 +0530

    Some more samples

--- Lists.py
+++ Lists.py
@@ -12,4 +12,20 @@ print(f"after poping left {quex}")
 quex.appendleft(10)
 print(f"after adding to left {quex}")
 
+list1 = [1,2,3,4,5]
+list2 = [4,5,6,7]
+templist = list1
+print(id(list1))
+list1 += list2
+print(id(list1))
+list1 += [8]
+list1.append(9)
+list1.insert(12,10)
+templist.insert(13,11)
+print(list1)
+print(f"This is temp list {templist}")
+print(id(templist))
+print(id(list1))
+
+
 
