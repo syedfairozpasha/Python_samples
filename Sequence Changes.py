1a204c5823f72caf3b1b067f1b117f4ee6714be4
sfp_BrainFry <49707987+syedfairozpasha@users.noreply.github.com>
2021-06-15 00:07:19 +0530

    lists and navigation

--- Sequence Changes.py
+++ Sequence Changes.py
@@ -16,3 +16,8 @@ print(f"after deleting{list2}")
 elec_gadget = ["computer","Music system","video player","Gaming Console"]
 for gindex,gitem in enumerate(elec_gadget):
     print(f"The item id is {gindex+1} and item is {gitem}")
+
+elec_gadget2 = ["computer","Music system","video player","Gaming Console"]
+elec_applrat = ["2 star","3 star","4 star","5 star","No star"]
+for g1,g2 in zip(sorted(elec_gadget2),reversed(elec_applrat)):
+    print(f"The electronic gadget is {g1} and rating is {g2}")
